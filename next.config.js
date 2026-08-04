9113726009351ebc81292ac38c4098e3d5962ba1
Adam Sjöström <horselservice@gmail.com>
2026-08-04 23:30:52 +0200

    allow images from supabase

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const supabaseHostname = supabaseUrl
+  ? new URL(supabaseUrl).hostname
+  : null;
+
 module.exports = {
   images: {
     unoptimized: true,
+
+    remotePatterns: supabaseHostname
+      ? [
+          {
+            protocol: "https",
+            hostname: supabaseHostname,
+            pathname:
+              "/storage/v1/object/public/**",
+          },
+        ]
+      : [],
   },
-}
+};
